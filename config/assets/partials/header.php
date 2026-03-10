1d9467ce920ee65de73646db4c0b02a7d12001cf
mkethichack-eng <mk.ethichack@gmail.com>
2026-03-10 16:46:14 +0530

    Create header.php

---
+++ config/assets/partials/header.php
@@ -0,0 +1,9 @@
+<div class="top">UPOSK Union — Admin Panel</div>
+<div class="nav">
+ <a href="index.php">Dashboard</a>
+ <a href="complaints.php">Complaints</a>
+ <a href="news.php">News</a>
+ <a href="members.php">Members</a>
+ <a href="logout.php">Logout</a>
+</div>
+<div class="wrap">
